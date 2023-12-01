620e0f07b4465889b1e54c2ce228f11ef1723fc3
hxntai.sh <juliettepoisson3@gmail.com>
2023-12-01 22:27:18 +0100

    1.0.2

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ else:
 
 os.system('cls' if os.name == 'nt' else 'clear')
 
-print('© raven.ovh | 2023')
+print('\033[1;31;40m' + '© raven.ovh | 2023 | Pinger' + '\033[0;37;40m')
 time.sleep(2)
 
 os.system('cls' if os.name == 'nt' else 'clear')
@@ -18,7 +18,7 @@ os.system('cls' if os.name == 'nt' else 'clear')
 def loading():
     for i in range(0, 101):
         time.sleep(0.01)
-        sys.stdout.write("\r" + "[+] Loading modules: %d%%" % i)
+        sys.stdout.write("\r" + '\033[1;35;40m' + "[+] Loading modules: " + str(i) + "%" + '\033[0;37;40m')
         sys.stdout.flush()
     print("\n")
 
@@ -27,21 +27,21 @@ loading()
 
 os.system('cls' if os.name == 'nt' else 'clear')
 
-print('© raven.ovh | What do you wanna ping ?')
-print('1. Website')
-print('2. IP')
-choice = input('Choice: ')
+print('\033[1;31;40m' + '© raven.ovh | What do you wanna ping ?' + '\033[0;37;40m')
+print('\033[1;36;40m' + '1. Website' + '\033[0;37;40m')
+print('\033[1;36;40m' + '2. IP' + '\033[0;37;40m')
+choice = input('\033[1;31;40m' + 'Choice: ' + '\033[0;37;40m')
 
 if choice == '1':
     os.system('cls' if os.name == 'nt' else 'clear')
-    print('Example: google.com')
-    website = input('Website: ')
+    print('\033[1;36;40m' + 'Example: google.com' + '\033[0;37;40m')
+    website = input('\033[1;31;40m' + 'Website: ' + '\033[0;37;40m')
     os.system('cls' if os.name == 'nt' else 'clear')
     os.system('ping ' + website + ' -t' if os.name == 'nt' else 'ping ' + website)
 elif choice == '2':
     os.system('cls' if os.name == 'nt' else 'clear')
-    print('Example: 1.1.1.1')
-    ip = input('IP: ')
+    print('\033[1;36;40m' + 'Example: 1.1.1.1' + '\033[0;37;40m')
+    ip = input('\033[1;31;40m' + 'IP: ' + '\033[0;37;40m')
     os.system('cls' if os.name == 'nt' else 'clear')
     os.system('ping ' + ip + ' -t' if os.name == 'nt' else 'ping ' + ip)
 else:
